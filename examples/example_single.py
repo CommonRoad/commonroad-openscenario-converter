50fb904821372ec27c9d5d33811da1869726e29f
yuanfei <yuanfei.lin@tum.de>
2023-05-08 18:13:19 +0200

    add scenario

--- examples/example_single.py
+++ examples/example_single.py
@@ -5,7 +5,11 @@ from osc_cr_converter.converter.osc2cr import Osc2CrConverter
 from osc_cr_converter.utility.visualization import visualize_scenario
 
 scenario_path = os.path.dirname(os.path.realpath(__file__)) + '/../scenarios/from_esmini/xosc/'
-openscenario = 'pedestrian.xosc'
+# openscenario = 'pedestrian.xosc'
+openscenario = 'pedestrian_collision.xosc'
+
+# scenario_path = os.path.dirname(os.path.realpath(__file__)) + '/../scenarios/from_openScenario_standard/'
+# openscenario = 'DoubleLaneChanger.xosc'
 config = ConverterParams.load(os.path.dirname(os.path.realpath(__file__)) +
                               '/../configurations/' + openscenario.replace('.xosc', '.yaml'))
 converter = Osc2CrConverter(config)
