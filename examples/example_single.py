44dbe24f12f470e94b033ac8d6f8b99ba4fe0d89
yuanfei <yuanfei.lin@tum.de>
2023-05-08 17:59:48 +0200

    add example for pedestrian scenario

--- examples/example_single.py
+++ examples/example_single.py
@@ -2,8 +2,13 @@
 import os
 from osc_cr_converter.utility.configuration import ConverterParams
 from osc_cr_converter.converter.osc2cr import Osc2CrConverter
+from osc_cr_converter.utility.visualization import visualize_scenario
 
-scenario_path = os.path.dirname(os.path.realpath(__file__)) + '/scenarios/from_esmini/xosc/pedestrian.xosc'
-config = ConverterParams()
+scenario_path = os.path.dirname(os.path.realpath(__file__)) + '/../scenarios/from_esmini/xosc/'
+openscenario = 'pedestrian.xosc'
+config = ConverterParams.load(os.path.dirname(os.path.realpath(__file__)) +
+                              '/../configurations/' + openscenario.replace('.xosc', '.yaml'))
 converter = Osc2CrConverter(config)
-scenario = converter.run_conversion(scenario_path)
+scenario = converter.run_conversion(scenario_path + openscenario)
+visualize_scenario(scenario, config)
+
