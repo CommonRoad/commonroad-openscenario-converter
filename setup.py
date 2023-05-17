f21d44644ea5e2388858ff874682a2cedbee46b8
yuanfei <yuanfei.lin@tum.de>
2023-05-17 09:43:23 +0200

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='commonroad-openscenario-converter',
-      version='0.0.1',
+      version='0.0.2',
       description='Converter between OpenSCENARIO and CommonRoad formats',
       keywords="scenario description, autonomous driving",
       author='Yuanfei Lin, Michael Ratzel',
