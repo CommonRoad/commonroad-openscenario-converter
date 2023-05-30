29cff75293a54f1a36247d847748ba3077090946
yuanfei <yuanfei.lin@tum.de>
2023-05-30 12:34:30 +0200

    fix pedestrian length

--- osc_cr_converter/converter/osc2cr.py
+++ osc_cr_converter/converter/osc2cr.py
@@ -348,7 +348,7 @@ class Osc2CrConverter(Converter):
             # for pedestrian, we consider an overapproximated circular area.
             # see: Koschi, Markus, et al. "Set-based prediction of pedestrians in urban environments considering
             # formalized traffic rules." IEEE ITSC, 2018
-            shape = Circle(max(states[0].get_object_length(), states[0].get_object_width())/2.)
+            shape = Circle(max(states[0].get_object_length()/2., states[0].get_object_width())/2.)
         else:
             shape = Rectangle(states[0].get_object_length(), states[0].get_object_width())
 
