5c9185725974f4d3c9086b21fa3879cf6b7844fb
yuanfei <yuanfei.lin@tum.de>
2023-06-15 15:40:44 +0200

    fix spacing

--- examples/example_single.py
+++ examples/example_single.py
@@ -1,4 +1,3 @@
-
 import os
 from osc_cr_converter.utility.configuration import ConverterParams
 from osc_cr_converter.converter.osc2cr import Osc2CrConverter
@@ -8,10 +7,9 @@ import osc_cr_converter.utility.logger as util_logger
 scenario_path = os.path.dirname(os.path.realpath(__file__)) + '/../scenarios/from_esmini/xosc/'
 openscenario = 'pedestrian_collision.xosc'
 config = ConverterParams.load(os.path.dirname(os.path.realpath(__file__)) +
-                               '/../configurations/' + openscenario.replace('.xosc', '.yaml'))
+                              '/../configurations/' + openscenario.replace('.xosc', '.yaml'))
 util_logger.initialize_logger(config)
 
 converter = Osc2CrConverter(config)
 scenario = converter.run_conversion(scenario_path + openscenario)
 visualize_scenario(scenario, config)
-
