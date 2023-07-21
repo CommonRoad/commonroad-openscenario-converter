e52260e379752760191d8954ec0aa1d8102498a4
yuanfei <yuanfei.lin@tum.de>
2023-07-21 16:36:13 +0200

    update pypi setup

--- setup.py
+++ setup.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python
 
+import os
 from setuptools import setup, find_packages
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(this_directory, 'README.md'), 'r', encoding='utf-8') as f:
+    readme = f.read()
+
 
 setup(name='commonroad-openscenario-converter',
       version='0.0.4',
       description='Converter between OpenSCENARIO and CommonRoad formats',
       keywords="scenario description, autonomous driving",
+      long_description_content_type='text/markdown',
+      long_description=readme,
+      url='https://commonroad.in.tum.de/tools/openscenario-converter',
+      project_urls={
+          'Documentation': '',
+          'Forum': 'https://commonroad.in.tum.de/forum/c/dataset-converter/',
+          'Source': 'https://gitlab.lrz.de/tum-cps/commonroad-openscenario-converter',
+      },
       author='Yuanfei Lin, Michael Ratzel',
       author_email='yuanfei.lin@tum.de',
       license='BSD 3-Clause',
+      data_files=[('.', ['LICENSE'])],
       packages=find_packages(),
       install_requires=[
           'commonroad-io==2023.1',
